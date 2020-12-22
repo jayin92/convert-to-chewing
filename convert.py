8fbbe58b8ad91040ff196cf89b52be07816bec3d
jayin92 <jayin920805@gmail.com>
2020-12-22 10:52:24 +0800

    real-time convert

--- convert.py
+++ convert.py
@@ -1,14 +1,22 @@
-path = input("Text file path >")
+from time import sleep
+# path = input("Text file path >")
 
-with open(path, "r") as file:
-    data = file.read()
 
-alpha   = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-"
-chewing = "ㄅㄆㄇㄈㄉㄊㄋㄌˇㄍㄎㄏˋㄐㄑㄒㄓㄔㄕㄖˊㄗㄘㄙ˙ㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ"
 
-for idx in range(len(alpha)):
-    data = data.replace(alpha[idx], chewing[idx])
+path = "input.txt"
 
-with open("output.txt", "w") as file:
-    file.write(data)
+while True:
+    with open(path, "r") as file:
+        data = file.read()
+
+    alpha   = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-"
+    chewing = "ㄅㄆㄇㄈㄉㄊㄋㄌˇㄍㄎㄏˋㄐㄑㄒㄓㄔㄕㄖˊㄗㄘㄙ˙ㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ"
+
+    for idx in range(len(alpha)):
+        data = data.replace(alpha[idx], chewing[idx])
+
+    with open("output.txt", "w") as file:
+        file.write(data)
+    
+    sleep(1)
 
